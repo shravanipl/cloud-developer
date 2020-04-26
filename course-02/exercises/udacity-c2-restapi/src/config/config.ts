f692fea55f04d57fcf081703efef33c83606b533
shravanipl <pl.shravani@gmail.com>
2020-04-26 01:11:58 -0500

    Using bcrypt for passwords

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -4,11 +4,14 @@ export const config = {
 		password: process.env.POSTGRES_PASSWORD,
 		database: process.env.POSTGRES_DATABASE,
 		host: process.env.POSTGRES_HOST,
-		dialect: "postgres"
+		dialect: 'postgres',
 	},
 	aws: {
 		aws_region: process.env.aws_region,
 		aws_profile: process.env.aws_profile,
 		aws_media_bucket: process.env.aws_media_bucket,
+	},
+	jwt: {
+		secret: 'Hello world',
 	},
 };
