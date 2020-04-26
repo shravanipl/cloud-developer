e4079b3eb6f580e7a74b1b2a1838fa8a76d26c67
shravanipl <pl.shravani@gmail.com>
2020-04-25 22:48:45 -0500

    Deploy using elastic beanstalk

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,14 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+	postgres: {
+		username: process.env.POSTGRES_USERNAME,
+		password: process.env.POSTGRES_PASSWORD,
+		database: process.env.POSTGRES_DATABASE,
+		host: process.env.POSTGRES_HOST,
+		dialect: "postgres"
+	},
+	aws: {
+		aws_region: process.env.aws_region,
+		aws_profile: process.env.aws_profile,
+		aws_media_bucket: process.env.aws_media_bucket,
+	},
+};
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -7,93 +7,88 @@ const router: Router = Router();
 
 // Get all feed items
 router.get('/', async (req: Request, res: Response) => {
-    const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
-    items.rows.map((item) => {
-            if(item.url) {
-                item.url = AWS.getGetSignedUrl(item.url);
-            }
-    });
-    res.send(items);
+	const items = await FeedItem.findAndCountAll({ order: [['id', 'DESC']] });
+	//Mapping the item with AWS getsignedURL for the URL stored in DB
+	items.rows.map((item) => {
+		if (item.url) {
+			//Taking key from DB and get signedURL from S3 so resource can be accessed directly from our client
+			item.url = AWS.getGetSignedUrl(item.url);
+		}
+	});
+	res.send(items);
 });
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
 
 router.get('/:id', async (req: Request, res: Response) => {
-    const { id } = req.params;
-        const item = await FeedItem.findByPk(id);
-    if (item != null) {
-        res.send(item);
-    }
-    
-    else 
-        res.status(404).send("Not found");
+	const { id } = req.params;
+	const item = await FeedItem.findByPk(id);
+	if (item != null) {
+		res.send(item);
+	} else res.status(404).send('Not found');
 });
 
 // update a specific resource
-router.patch('/:id',
-    requireAuth,
-    async (req: Request, res: Response) => {
-        //@TODO try it yourself
-        const id = req.params.id;
-        const caption = req.body.caption;
-        const url = req.body.url;
-        if (!caption || !url) {
-            res.send(404).send("Caption or URL is missing");
-        }
-        else {
-            const item = await FeedItem.findByPk(id);
-            if (item != null) {
-                item.update({ url: url, caption: caption })
-                    .then(() => {
-                        console.log("Updated");
-                        res.status(200).send(item);
-                    });
-            }
-            else {
-                res.send("Item couldn't be updated");
-            }
-        }
+router.patch('/:id', requireAuth, async (req: Request, res: Response) => {
+	//@TODO try it yourself
+	const id = req.params.id;
+	const caption = req.body.caption;
+	const url = req.body.url;
+	if (!caption || !url) {
+		res.send(404).send('Caption or URL is missing');
+	} else {
+		const item = await FeedItem.findByPk(id);
+		if (item != null) {
+			item.update({ url: url, caption: caption }).then(() => {
+				console.log('Updated');
+				res.status(200).send(item);
+			});
+		} else {
+			res.send("Item couldn't be updated");
+		}
+	}
 });
-
 
 // Get a signed url to put a new item in the bucket
-router.get('/signed-url/:fileName', 
-    requireAuth, 
-    async (req: Request, res: Response) => {
-    let { fileName } = req.params;
-    const url = AWS.getPutSignedUrl(fileName);
-    res.status(201).send({url: url});
-});
+router.get(
+	'/signed-url/:fileName',
+	requireAuth,
+	async (req: Request, res: Response) => {
+		let { fileName } = req.params;
+		const url = AWS.getPutSignedUrl(fileName);
+		res.status(201).send({ url: url });
+	}
+);
 
-// Post meta data and the filename after a file is uploaded 
+// Post meta data and the filename after a file is uploaded
 // NOTE the file name is they key name in the s3 bucket.
 // body : {caption: string, fileName: string};
-router.post('/', 
-    requireAuth, 
-    async (req: Request, res: Response) => {
-    const caption = req.body.caption;
-    const fileName = req.body.url;
+router.post('/', requireAuth, async (req: Request, res: Response) => {
+	const caption = req.body.caption;
+	const fileName = req.body.url;
 
-    // check Caption is valid
-    if (!caption) {
-        return res.status(400).send({ message: 'Caption is required or malformed' });
-    }
+	// check Caption is valid
+	if (!caption) {
+		return res
+			.status(400)
+			.send({ message: 'Caption is required or malformed' });
+	}
 
-    // check Filename is valid
-    if (!fileName) {
-        return res.status(400).send({ message: 'File url is required' });
-    }
+	// check Filename is valid
+	if (!fileName) {
+		return res.status(400).send({ message: 'File url is required' });
+	}
 
-    const item = await new FeedItem({
-            caption: caption,
-            url: fileName
-    });
+	const item = await new FeedItem({
+		caption: caption,
+		url: fileName,
+	});
 
-    const saved_item = await item.save();
+	const saved_item = await item.save();
 
-    saved_item.url = AWS.getGetSignedUrl(saved_item.url);
-    res.status(201).send(saved_item);
+	saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+	res.status(201).send(saved_item);
 });
 
 export const FeedRouter: Router = router;
